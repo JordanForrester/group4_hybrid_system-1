de09e2bc9db58281d75ea211ffc8d9bcb965aa94
Dexorp <90629112+JordanForrester@users.noreply.github.com>
2024-03-02 10:48:53 -0500

    upd

--- Jordan/Services/emailClass.py
+++ Jordan/Services/emailClass.py
@@ -2,7 +2,9 @@
 
 
 class Email:
-    title = ""
+    subject = ""
+    sender = ""
+    receiver = ""
     message = ""
     id = 0
     header = []
@@ -11,6 +13,16 @@ class Email:
         self.id = id
         self.header = header
         self.message = message
+
+        for entry in header:
+            if entry['name'] == "From":
+                self.sender = entry['value']
+            if entry['name'] == "To":
+                self.receiver = entry['value']
+            if entry['name'] == "Subject":
+                self.subject = entry['value']
+        print( "From: "+ self.sender)
+
 
 
 
